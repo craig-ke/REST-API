ea796aa54f39e253158e322c2eb87f29c2dab6d2
craig-ke <lawremce.olonyi.67@gmail.com>
2020-12-14 02:18:42 +0300

    1st 2nd and 3rd test pass

--- src/test/java/models/NewsTest.java
+++ src/test/java/models/NewsTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class NewsTest {
     //setup
@@ -15,12 +16,27 @@ public class NewsTest {
     @After
     public void tearDown() throws Exception {
     }
-//1st test
-@Test
-public void getGeneral() {
-    News testNews = setupNews();
-    assertEquals("Economy", testNews.getGeneral());
-}
+    //1st test
+    @Test
+    public void getGeneral() {
+        News testNews = setupNews();
+        assertEquals("Economy", testNews.getGeneral());
+    }
+    //2nd test
+    @Test
+    public void getDepartment() {
+        News testNews = setupNews();
+        assertEquals("Rainfall", testNews.getDepartment());
+    }
+    //3rd test
+    @Test
+    public void setGeneral() {
+        News testNews = setupNews();
+        testNews.setGeneral("water") ;
+        assertNotEquals("Economy", testNews.getGeneral());
+    }
+
+
 
     // helper
     public News  setupNews(){
