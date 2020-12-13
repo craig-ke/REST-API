31ef0b789a75efed6e4bb0fd2f3547e379f4725a
craig-ke <lawremce.olonyi.67@gmail.com>
2020-12-14 02:14:57 +0300

    add getter and setter for class News

--- src/test/java/models/NewsTest.java
+++ src/test/java/models/NewsTest.java
@@ -2,6 +2,9 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class NewsTest {
     //setup
@@ -12,5 +15,15 @@ public class NewsTest {
     @After
     public void tearDown() throws Exception {
     }
+//1st test
+@Test
+public void getGeneral() {
+    News testNews = setupNews();
+    assertEquals("Economy", testNews.getGeneral());
+}
 
+    // helper
+    public News  setupNews(){
+        return new News("Economy","Rainfall");
+    }
 }
