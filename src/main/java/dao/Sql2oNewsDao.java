25eaa55af9741383432d35016efdc8b629cbc836
craig-ke <lawremce.olonyi.67@gmail.com>
2020-12-14 04:03:57 +0300

    add override clearAll

--- src/main/java/dao/Sql2oNewsDao.java
+++ src/main/java/dao/Sql2oNewsDao.java
@@ -50,4 +50,13 @@ public class Sql2oNewsDao {
             System.out.println(ex);
         }
     }
+    @Override
+    public void clearAll() {
+        String sql = "DELETE from news";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql).executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
 }
