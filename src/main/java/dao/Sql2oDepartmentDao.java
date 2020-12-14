a518a8183225a233bfe3998537c9400f0e341b37
craig-ke <lawremce.olonyi.67@gmail.com>
2020-12-14 03:32:01 +0300

    add override boolean

--- src/main/java/dao/Sql2oDepartmentDao.java
+++ src/main/java/dao/Sql2oDepartmentDao.java
@@ -1,6 +1,9 @@
 package dao;
 
+import models.Departments;
+import org.sql2o.Connection;
 import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
 
 public class Sql2oDepartmentDao implements DepartmentsDao  {
     private final Sql2o sql2o;
@@ -8,4 +11,18 @@ public class Sql2oDepartmentDao implements DepartmentsDao  {
         this.sql2o = sql2o;
     }
 
+    @Override
+    public boolean equals(Departments departments {
+        String sql = "INSERT INTO departments (name, description, totalNumber) VALUES (:name, :description, :totalNumber)";
+        try (Connection con = sql2o.open()) {
+            int id = (int) con.createQuery(sql, true)
+                    .bind(departments)
+                    .executeUpdate()
+                    .getKey();
+            departments .setId(id);
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
 }
+
