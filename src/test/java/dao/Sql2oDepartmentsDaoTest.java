beb307218bd9c4279c504f8785e951d94dfb539d
craig-ke <lawremce.olonyi.67@gmail.com>
2020-12-14 10:40:16 +0300

    tets department sql2o pass apart from 9th test

--- src/test/java/dao/Sql2oDepartmentsDaoTest.java
+++ src/test/java/dao/Sql2oDepartmentsDaoTest.java
@@ -8,6 +8,9 @@ import org.junit.Test;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 public class Sql2oDepartmentsDaoTest {
@@ -36,7 +39,80 @@ public class Sql2oDepartmentsDaoTest {
         Departments testDepartments = setupDepartments();
         assertNotEquals(0, testDepartments.getId());
     }
-
+    //2nd test
+    @Test
+    public void addedDepartmentsAreReturnedFromGetAll() throws Exception {
+        Departments  testDepartments = setupDepartments();
+        Departments  otherDepartments = setupDepartments();
+        assertEquals(2, departmentsDao.getAll().size());
+    }
+    //3rd test
+    @Test
+    public void noDepartmentsReturnsEmptyList() throws Exception {
+        assertEquals(0, departmentsDao.getAll().size());
+    }
+    //4th test
+    @Test
+    public void findByIdReturnsCorrectDepartment() throws Exception {
+        Departments  testDepartments = setupDepartments();
+        Departments  otherDepartments = setupDepartments();
+        assertNotEquals(testDepartments , departmentsDao.findById(testDepartments.getId()));
+    }
+    //5th test
+    @Test
+    public void updateCorrectlyUpdatesAllFields() throws Exception {
+        Departments  testDepartments = setupDepartments();
+        departmentsDao.update(testDepartments.getId(), "a", "b", 400);
+        Departments  foundDepartments = departmentsDao.findById(testDepartments.getId());
+        assertEquals("a", foundDepartments.getName());
+        assertEquals("b", foundDepartments.getDescription());
+        assertEquals(400, foundDepartments.getTotalNumber());
+    }
+    //6th test
+    @Test
+    public void deleteByIdDeletesCorrectDepartment() throws Exception {
+        Departments  testDepartments = setupDepartments();
+        Departments  otherDepartments = setupDepartments();
+        departmentsDao.deleteById(testDepartments.getId());
+        assertEquals(1, departmentsDao.getAll().size());
+    }
+    //7th test
+    @Test
+    public void clearAll() throws Exception {
+        Departments  testDepartments = setupDepartments();
+        Departments  otherDepartments = setupDepartments();
+        departmentsDao.clearAll();
+        assertEquals(0, departmentsDao.getAll().size());
+    }
+    //8th test
+//    @Test
+//    public void DepartmentsReturnsNewsCorrectly() throws Exception {
+//        News testNews = setupNews();
+//        newsDao.add(testNews);
+//        News otherNews = setupNews();
+//        newsDao.add(otherNews);
+//        Departments  testDepartments = setupDepartments();
+//        departmentsDao.add(testDepartments);
+//        departmentsDao.addDepartmentsToNews(testDepartments ,testNews);
+//        departmentsDao.addDepartmentsToNews(testDepartments ,otherNews);
+//        News [] news = {testNews , otherNews};
+//        assertEquals(Arrays.asList(news), departmentsDao.getAllNewsByDepartments(testDepartments.getId()) );
+//    }
+     //9th test
+    @Test
+    public void deletingNewsAlsoUpdatesJoinTable() throws Exception {
+        Departments  testDepartments = setupDepartments();
+        departmentsDao.add(testDepartments);
+        News testNews = setupNews();
+        newsDao.add(testNews);
+        News anotherNews = setupNews();
+        newsDao.add(anotherNews);
+        newsDao.addNewsToDepartments(testNews ,testDepartments);
+        newsDao.addNewsToDepartments(anotherNews ,testDepartments);
+        newsDao.deleteById(testNews.getId());
+        newsDao.deleteById(anotherNews.getId());
+        assertEquals(0, newsDao.getAllDepartmentsByNews(testNews.getId()) .size());
+    }
     //Helpers
     public Departments  setupDepartments (){
         Departments  departments = new Departments("IT", "Programming", 500) ;
