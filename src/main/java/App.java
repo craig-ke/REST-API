289bf12fae21e23067fbee780ffc8b35823d3637
craig-ke <lawremce.olonyi.67@gmail.com>
2020-12-15 10:51:05 +0300

    add correction

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,7 +4,6 @@ import dao.Sql2oNewsDao;
 import dao.Sql2oDepartmentsDao;
 import models.Departments;
 import models.News;
-import models.News;
 import models.Users;
 import org.sql2o.Sql2o;
 import org.sql2o.Connection;
@@ -20,93 +19,73 @@ public class App {
         Gson gson = new Gson();
 
         String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/database.sql'";
-        Sql2o sql2o = new Sql2o(connectionString, "", " ");
+        Sql2o sql2o = new Sql2o(connectionString,""," ");
 
-        departmentDao = new Sql2oDepartmentsDao(sql2o);
+        departmentDao = new  Sql2oDepartmentsDao (sql2o);
         userDao = new Sql2oUsersDao(sql2o);
         newsDao = new Sql2oNewsDao(sql2o);
         departmentNewsDao = new Sql2oNewsDao(sql2o);
         conn = sql2o.open();
 
-//        post("/departments/new", "application/json", (req, res) -> {
-//            Departments department = gson.fromJson(req.body(), Departments.class);
-//            departmentDao.addDepartments(department);
-//            res.status(201);
-//            res.type("application/json");
-//            return gson.toJson(department);
-//        });
-//
-//        get("/departments", "application/json", (req, res) -> {
-//            res.type("application/json");
-//            return gson.toJson(departmentDao.getAllDepartments());
-//        });
-//
-//        get("/departments/:id", "application/json", (req, res) -> {
-//            res.type("application/json");
-//            int departmentId = Integer.parseInt(req.params("id"));
-//            res.type("application/json");
-//            return gson.toJson(departmentDao.findDepartmentById(departmentId));
-//        });
-//
-//        post("/users/new", "application/json", (req, res) -> {
-//            Users user = gson.fromJson(req.body(), Users.class);
-//            userDao.addUser(user);
-//            res.status(201);
-//            res.type("application/json");
-//            return gson.toJson(user);
-//        });
-//
-//        get("/users", "application/json", (req, res) -> {
-//            res.type("application/json");
-//            return gson.toJson(userDao.getAllUsers());
-//        });
-//
-//        get("/users/:id", "application/json", (req, res) -> {
-//            res.type("application/json");
-//            int userId = Integer.parseInt(req.params("id"));
-//            res.type("application/json");
-//            return gson.toJson(userDao.findUserById(userId));
-//        });
-//
-//        post("/news/new", "application/json", (req, res) -> {
-//            organizationGeneralNews news = gson.fromJson(req.body(), organizationGeneralNews.class);
-//            newsDao.addOrganizationGeneralNews(news);
-//            res.status(201);
-//            res.type("application/json");
-//            return gson.toJson(news);
-//        });
-//
-//        get("/news", "application/json", (req, res) -> {
-//            res.type("application/json");
-//            return gson.toJson(newsDao.getAllGeneralNews());
-//        });
-//
-//        get("/news/:id", "application/json", (req, res) -> {
-//            res.type("application/json");
-//            int newsId = Integer.parseInt(req.params("id"));
-//            res.type("application/json");
-//            return gson.toJson(newsDao.findGeneralNewsById(newsId));
-//        });
-//
-//        post("/departmentNews/new", "application/json", (req, res) -> {
-//           News departmentNews = gson.fromJson(req.body(), News.class);
-//            departmentNewsDao.DepartmentsNews(departmentNews);
-//            res.status(201);
-//            res.type("application/json");
-//            return gson.toJson(departmentNews);
-//        });
-//
-//        get("/news", "application/json", (req, res) -> {
-//            res.type("application/json");
-//            return gson.toJson(departmentNewsDao.getAllDepartmentNews());
-//        });
-//
-//        get("/news/:id", "application/json", (req, res) -> {
-//            res.type("application/json");
-//            int newsId = Integer.parseInt(req.params("id"));
-//            res.type("application/json");
-//            return gson.toJson(departmentNewsDao.findDepartmentNewsById(newsId));
-//        });
-//    }
+        post("/departments/new", "application/json", (req, res) -> {
+            Departments department = gson.fromJson(req.body(), Departments.class);
+            departmentDao.add(department);
+            res.status(201);
+            res.type("application/json");
+            return gson.toJson(department);
+        });
+
+        get("/departments", "application/json", (req, res) -> {
+            res.type("application/json");
+            return gson.toJson(departmentDao.getAll());
+        });
+
+        get("/departments/:id", "application/json", (req, res) -> {
+            res.type("application/json");
+            int departmentId = Integer.parseInt(req.params("id"));
+            res.type("application/json");
+            return gson.toJson(departmentDao.getAllNewsByDepartments(departmentId));
+        });
+
+        post("/users/new", "application/json", (req, res) -> {
+            Users user = gson.fromJson(req.body(), Users.class);
+            userDao.add(user);
+            res.status(201);
+            res.type("application/json");
+            return gson.toJson(user);
+        });
+
+        get("/users", "application/json", (req, res) -> {
+            res.type("application/json");
+            return gson.toJson(userDao.getAll());
+        });
+
+        get("/users/:id", "application/json", (req, res) -> {
+            res.type("application/json");
+            int userId = Integer.parseInt(req.params("id"));
+            res.type("application/json");
+            return gson.toJson(userDao.getAllUsersByDepartments(userId));
+        });
+
+        post("/news/new", "application/json", (req, res) -> {
+            News news = gson.fromJson(req.body(), News.class);
+            newsDao.add(news);
+            res.status(201);
+            res.type("application/json");
+            return gson.toJson(news);
+        });
+
+        get("/news", "application/json", (req, res) -> {
+            res.type("application/json");
+            return gson.toJson(newsDao.getAll());
+        });
+
+        get("/news/:id", "application/json", (req, res) -> {
+            res.type("application/json");
+            int newsId = Integer.parseInt(req.params("id"));
+            res.type("application/json");
+            return gson.toJson(newsDao.getAllDepartmentsByNews(newsId));
+        });
+
     }
 }
